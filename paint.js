6abbd358aa2b1798b6692e5c4030f30adb65a043
ThijsDollekens <87063168+ThijsDollekens@users.noreply.github.com>
2021-08-13 10:54:39 +0200

    Add files via upload

---
+++ paint.js
@@ -0,0 +1,18 @@
+const wallColor = "red"
+const selectWall = "north"
+
+const color  = function(item){
+    console.log("The "+ selectWall +" wall has been painted " + item)
+};
+
+const color1 = "red";
+const color2 = "green";
+
+
+if (wallColor == "red"){
+    color(color1);
+}
+if (wallColor == "green"){   
+    color(color2);
+}
+
